56191b9fd549f2b89a8d272ac173425fe95252c7
Phillip Gatlin <42885791+PHILLIPGATLIN@users.noreply.github.com>
2018-09-06 19:09:55 -0700

    Update editor.c

--- editor.c
+++ editor.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#include <termios.h>
-#include <unistd.h>
 unsigned int row=0, column=0;
 char page[1000][1000];
-unsigned int istext(int);
-unsigned int istext(int c)
-{
-	if ((c >= 0x20 && c <= 0x7E) || c == 0x09)
-		return 1;
-	else
-		return 0;
-}
 void drawpage(void);
 void drawpage ()
 {
@@ -111,8 +101,7 @@ int main(int argc, char *argv[])
 		       "e.g. txt /home/terry/document.txt\n");
 		return -1;
 	}
-	int n=0, pger=0, pgec=0, kbd=0;
-	int c=0;
+	int n=0, c=0;
 	FILE *file;
 	if ((file = fopen(argv[1], "a+")) == NULL)
 	{
