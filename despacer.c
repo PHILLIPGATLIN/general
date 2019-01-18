cc7019cdebfe586ed05959c12652f52872240d3e
Phillip Gatlin <42885791+PHILLIPGATLIN@users.noreply.github.com>
2019-01-17 16:44:18 -0800

    Add files via upload

---
+++ despacer.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+char old[255];
+char new[255];
+char c;
+int despacer(int, char**);
+int despacer(int agindex, char** ags)
+{
+    if (agindex == 0)
+        return 1;
+    int index1 = 0;
+    int index2 = 0;
+    strcpy(old, ags[agindex]);
+    for (index1 = 0, index2 = 0; (c = old[index1]) != '\0'; index1++)
+    {
+        if (c == ' ' || c == '\t' || c == 39 || c == '(' || c == ')')
+            continue;
+        else
+        {
+            new[index2] = c;
+            index2++;
+        }
+    }
+    rename(old, new);
+    agindex = agindex - 1;
+    memset(old, 0, 255);
+    memset(new, 0, 255);
+    despacer(agindex,ags);
+}
+int main(int argc, char* argv[])
+{
+    if (argc == 1)
+        return 1;
+    memset(old, 0, 255);
+    memset(new, 0, 255);
+    c = 0;
+    argc = argc - 1;
+    despacer(argc, argv);
+    return 1;
+}
